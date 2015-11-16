ad79bb09c037477e62d3f2b515e355c5e5817820
Tasveer Singh <taz@tazsingh.com>
2015-11-16 09:50:02 -0500

    Add HTML Toronto to Upcoming Events

--- upcomingEvents/GetUpcomingEvents.js
+++ upcomingEvents/GetUpcomingEvents.js
@@ -10,6 +10,7 @@ const javaScriptMeetups = {
   angularjs: makeGoogleCalendarURL("tgh4uc5t6uhr4icjrcgqfhe18r2uu3fg@import.calendar.google.com")
 , emberjs: makeGoogleCalendarURL("11j5qfhbb916srru7kuae99i4rn3p8r5@import.calendar.google.com")
 , fullstack: makeGoogleCalendarURL("qp59hmmpclktmd2ujqsunb6a8k4ferir@import.calendar.google.com")
+, htmlto: makeGoogleCalendarURL("89aheia1si29mqt1kvuprggnid983m87@import.calendar.google.com")
 , meteorjs: makeGoogleCalendarURL("sv5rg9q32cg6qhabdgi33fjur45vcilh@import.calendar.google.com")
 , reactjs: makeGoogleCalendarURL("59s1qmiqr7bo98uqkek5ba7er2eduk3t@import.calendar.google.com")
 , torontojs: makeGoogleCalendarURL("k6l8oiu416ftcjpjetn0r7a79me8pq4r@import.calendar.google.com")
