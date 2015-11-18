ddb1356d6738bc6893bf838aa3590713bbfe1b6e
Tasveer Singh <taz@tazsingh.com>
2015-11-17 22:52:45 -0500

    Put entire Google Analytics init in environment conditional

--- analytics.js
+++ analytics.js
@@ -1,15 +1,19 @@
-// google analytics tracking code with `window` replace with `module.exports`
-(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,"script","//www.google-analytics.com/analytics.js","ga");
-
-let ga = function(...args) {
-  window.ga.apply(window.ga, args);
-};
+let ga;
 
 if(process.env.NODE_ENV === "production") {
+  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+
+  ga = function(...args) {
+    window.ga.apply(window.ga, args);
+  };
+
   ga("create", "UA-70285153-1", "auto");
+}
+else {
+  ga = function() {}; // noop
 }
 
 export default ga;
