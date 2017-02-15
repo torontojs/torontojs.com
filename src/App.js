281d9d1d9b112e226cf5c3353738693182fc97a2
Alex Wilmer <alex.wilmer@oicr.on.ca>
2017-02-15 11:39:10 -0500

    🔧 add CNAME file

--- src/App.js
+++ src/App.js
@@ -3,34 +3,40 @@ import Splash from './components/Splash'
 import Events from './components/Events'
 import { BrowserRouter, Match, Miss } from 'react-router'
 import theme from './theme'
+//
+// let App = () =>
+//   <BrowserRouter>
+//     <div>
+//       <Match
+//         exactly pattern="/"
+//         component={() =>
+//           <div>
+//             <Splash />
+//             <Events />
+//           </div>
+//         }
+//       />
+//       <Match
+//         exactly pattern="/workshop"
+//         component={() =>
+//           <div>
+//             <Splash
+//               backgroundColor={theme.secondary}
+//               page="WORKSHOP"
+//             />
+//           </div>
+//         }
+//       />
+//       <Miss
+//         component={() => <div>heyo 404</div>}
+//       />
+//     </div>
+//   </BrowserRouter>
 
 let App = () =>
-  <BrowserRouter>
-    <div>
-      <Match
-        exactly pattern="/"
-        component={() =>
-          <div>
-            <Splash />
-            <Events />
-          </div>
-        }
-      />
-      <Match
-        exactly pattern="/workshop"
-        component={() =>
-          <div>
-            <Splash
-              backgroundColor={theme.secondary}
-              page="WORKSHOP"
-            />
-          </div>
-        }
-      />
-      <Miss
-        component={() => <div>heyo 404</div>}
-      />
-    </div>
-  </BrowserRouter>
+  <div>
+    <Splash />
+    <Events />
+  </div>
 
 export default App
