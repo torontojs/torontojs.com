585ba1e3ee6a2c7e806059ad728507bbc8208009
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2017-04-29 20:10:54 +0000

    Fixing issues re: @alex-wilmer recommendations (#18)

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import Splash from './Splash'
+
+const Page = styled.section`
+  padding: 20px;
+  max-width: 750px;
+  margin: 0 auto;
+`
 
 let ErrorPage = () => (
   <div>
     <Splash />
-    <div>
-      Uh oh! Something went wrong.
-    </div>
-    <div>
-      [Insert witty gif here]
-    </div>
+    <Page>
+      <p>Uh oh! Something went wrong.</p>
+      <p>[Insert witty gif here]</p>
+    </Page>
   </div>
 )
 
--- src/components/StaticPage.js
+++ src/components/StaticPage.js
@@ -1,61 +1,29 @@
 import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown'
-import { css } from 'glamor'
+import styled from 'styled-components'
 import Splash from './Splash'
 
+const basePath = process.env.NODE_ENV === `development` ? ``
+  : `https://raw.githubusercontent.com/torontojs/torontojs.com/gh-pages`
+
+const Page = styled.section`
+  padding: 20px;
+  max-width: 750px;
+  margin: 0 auto;
+`
+
 class StaticPage extends Component {
-  constructor (props) {
-    super(props)
-    
-    let page = ``
+  state = { content: `` }
 
-    // Lets not allow people to browse the file system,
-    //  only use the word in the path
-    //
-    // props.params is an Object... but has array like
-    //  keys (0, 1, etc) very weird way to parse params, 
-    //  I dont think I'm doing this correctly
-    if (props.match && 
-      props.match.params && 
-      props.match.params.hasOwnProperty(`page`)) {
-      
-      let buffer = props.match.params.page.match(/\w+/g)
-      
-      if (buffer.length > 0) {
-        page = buffer[0]
-      } else {
-        // Or error out?
-        this.state.history.replace({ pathname: `/404` })
-      }
-    }
- 
-    this.state = {
-      page,
-      content: ``,
-      history: props.history || {},
-    }  
-  }
-
-  componentDidMount () {
-    // Make request to get the data
-    fetch(`/${this.state.page}.md`)
+  async componentDidMount () {
+    let data = await fetch(`${basePath}/${this.props.match.params.page}.md`)
       .then((response) => response.text())
-      .then((data) => {
-        // Hack to work around react-router inability to send 404 status
-        // code for 404s. If we see a <head> tag in the response, its probably
-        // not Markdown...
-        if (data.match(/<head>/g)) {
-          throw new Error(`404 Not Found`)
-        }
 
-        // Update the state with the content so we re-render
-        this.setState({ content: data })
-      })
-      .catch(() => {
-        const { history } = this.state
-        // Or error out?
-        history.replace({ pathname: `/404` })
-      })
+    if (data.match(/<head>/g)) {
+      this.props.history.replace({ pathname: `/404` })
+    } else {
+      this.setState({ content: data })
+    }
   }
 
   render () {
@@ -64,18 +32,12 @@ class StaticPage extends Component {
     return (
       <div>
         <Splash />
-        <div className={page}>
+        <Page>
           <ReactMarkdown source={content} />
-        </div>
+        </Page>
       </div>
     )
   }
 }
-
-const page = css({
-  padding: `20px`,
-  maxWidth: `750px`,
-  margin: `0 auto`,
-})
 
 export default StaticPage
