3d9179fdbcbe36cc0dab7af6d21e4e8ef0ae6c7e
Navin Seth <navin.seth@mark43.com>
2019-02-07 10:46:59 -0500

    added Speaker Button

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,21 +1,21 @@
 // @flow
 
-import React from 'react'
-import styled from 'styled-components'
-import theme from '../theme'
-import ReactRotatingText from './RotatingText'
-import Logo from './svg/Logo'
-import SlackIcon from './svg/SlackIcon'
-import Octocat from './svg/Octocat'
+import React from "react";
+import styled from "styled-components";
+import theme from "../theme";
+import ReactRotatingText from "./RotatingText";
+import Logo from "./svg/Logo";
+import SlackIcon from "./svg/SlackIcon";
+import Octocat from "./svg/Octocat";
 
 let Row = styled.div`
   display: flex;
-`
+`;
 
 let Header = styled(Row)`
   position: relative;
   z-index: 1;
-`
+`;
 
 let Link = styled.a`
   color: white;
@@ -26,7 +26,7 @@ let Link = styled.a`
   &:hover {
     color: rgb(255, 248, 84);
   }
-`
+`;
 
 let LinkText = styled.span`
   font-weight: bold;
@@ -35,12 +35,12 @@ let LinkText = styled.span`
   @media (max-width: 700px) {
     display: none;
   }
-`
+`;
 
 let InnerContainer = styled.div`
   margin: 0 auto;
   max-width: 1000px;
-`
+`;
 
 let TitleContainer = styled.div`
   display: flex;
@@ -53,53 +53,68 @@ let TitleContainer = styled.div`
   z-index: 1;
   position: relative;
   letter-spacing: 4px;
-`
+`;
 
 let Container = styled.div`
   height: 80vh;
   background-color: ${props => props.backgroundColor || theme.primary};
   position: relative;
-`
+`;
 
 let Tower = styled(Logo)`
   position: absolute;
   bottom: 0;
   max-width: 600px;
-`
+`;
 
 let Title = styled.div`
   font-size: 35px;
   margin-bottom: 10px;
-`
+`;
 
 let Rotator = styled(ReactRotatingText)`
   font-size: 26px;
-`
+`;
 
 type Props = {
   backgroundColor?: string,
-  page?: string,
-}
+  page?: string
+};
 
-export default ({ backgroundColor = theme.primary, page = `TORONTO` }: Props) => (
+export default ({
+  backgroundColor = theme.primary,
+  page = `TORONTO`
+}: Props) => (
   <Container backgroundColor={backgroundColor}>
     <Header>
       <Link href="http://slack.torontojs.com/" target="_blank">
         <SlackIcon style={{ width: `30px` }} />
         <LinkText>Join us on Slack</LinkText>
       </Link>
+      <Link
+        href="https://docs.google.com/forms/d/e/1FAIpQLSc_ZA0ElHLsMedjdQjZechw5H1RJNhG1DZxtFTsqhkdut3eFg/viewform?usp=sf_link" // eslint-disable-line
+        target="_blank"
+      >
+        <i className="fa fa-microphone" style={{ fontSize: `32px` }} />
+        <LinkText>Join us on Meetup</LinkText>
+      </Link>
       <Link href="https://github.com/torontojs/torontojs.com" target="_blank">
         <Octocat width="30px" />
         <LinkText>Contribute to this site</LinkText>
       </Link>
-      <Link href="https://www.youtube.com/channel/UC1samyyfqiKmOT6fq3uVO1A" target="_blank">
+      <Link
+        href="https://www.youtube.com/channel/UC1samyyfqiKmOT6fq3uVO1A"
+        target="_blank"
+      >
         <i className="fa fa-youtube-play" style={{ fontSize: `32px` }} />
         <LinkText>Tech Talks</LinkText>
       </Link>
     </Header>
     <InnerContainer>
       <TitleContainer>
-        <Title>{page}&nbsp;<b style={{ color: `#ffffff` }}>JS</b></Title>
+        <Title>
+          {page}&nbsp;<b style={{ color: `#ffffff` }}>JS</b>
+        </Title>
         <Rotator
           items={[`MEETUPS`, `TECH TALKS`, `WORKSHOPS`, `SOCIAL EVENTS`]}
         />
@@ -107,4 +122,4 @@ export default ({ backgroundColor = theme.primary, page = `TORONTO` }: Props) =>
       <Tower fill={backgroundColor} />
     </InnerContainer>
   </Container>
-)
+);
