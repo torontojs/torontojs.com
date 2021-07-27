a545580e7f0f5a93903721ca98f3a271585514a9
jen chan <6406037+usrrname@users.noreply.github.com>
2021-07-27 07:53:24 -0400

    Link code of conduct to splash/landing page (#89)

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,84 +1,84 @@
 // @flow
 
-import React from 'react'
-import styled from 'styled-components'
-import theme from '../theme'
-import ReactRotatingText from './RotatingText'
-import Logo from './svg/Logo'
-import SlackIcon from './svg/SlackIcon'
-import Octocat from './svg/Octocat'
+import React from "react"
+import styled from "styled-components"
+import theme from "../theme"
+import ReactRotatingText from "./RotatingText"
+import Logo from "./svg/Logo"
+import SlackIcon from "./svg/SlackIcon"
+import Octocat from "./svg/Octocat"
 
 let Row = styled.div`
-  display: flex;
+	display: flex;
 `
 
 let Header = styled(Row)`
-  position: relative;
-  z-index: 1;
+	position: relative;
+	z-index: 1;
 `
 
 let Link = styled.a`
-  color: white;
-  padding: 10px;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  &:hover {
-    color: rgb(255, 248, 84);
-  }
+	color: white;
+	padding: 10px;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	&:hover {
+		color: rgb(255, 248, 84);
+	}
 `
 
 let LinkText = styled.span`
-  font-weight: bold;
-  padding-left: 10px;
-  transition: color 0.2s ease;
-  @media (max-width: 700px) {
-    display: none;
-  }
+	font-weight: bold;
+	padding-left: 10px;
+	transition: color 0.2s ease;
+	@media (max-width: 700px) {
+		display: none;
+	}
 `
 
 let InnerContainer = styled.div`
-  margin: 0 auto;
-  max-width: 1000px;
+	margin: 0 auto;
+	max-width: 1000px;
 `
 
 let TitleContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  font-family: ${theme.fancyFont};
-  color: white;
-  padding: 100px 0 25px;
-  z-index: 1;
-  position: relative;
-  letter-spacing: 4px;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	align-items: center;
+	font-family: ${theme.fancyFont};
+	color: white;
+	padding: 100px 0 25px;
+	z-index: 1;
+	position: relative;
+	letter-spacing: 4px;
 `
 
 let Container = styled.div`
-  height: 80vh;
-  background-color: ${props => props.backgroundColor || theme.primary};
-  position: relative;
+	height: 80vh;
+	background-color: ${(props) => props.backgroundColor || theme.primary};
+	position: relative;
 `
 
 let Tower = styled(Logo)`
-  position: absolute;
-  bottom: 0;
-  max-width: 600px;
+	position: absolute;
+	bottom: 0;
+	max-width: 600px;
 `
 
 let Title = styled.div`
-  font-size: 35px;
-  margin-bottom: 10px;
+	font-size: 35px;
+	margin-bottom: 10px;
 `
 
 let Rotator = styled(ReactRotatingText)`
-  font-size: 26px;
+	font-size: 26px;
 `
 
 type Props = {
-  backgroundColor?: string,
-  page?: string,
+	backgroundColor?: string,
+	page?: string,
 }
 
 const speakerFormId = `1FAIpQLSc_ZA0ElHLsMedjdQjZechw5H1RJNhG1DZxtFTsqhkdut3eFg`
@@ -87,30 +87,39 @@ const speakerFormLink = `https://docs.google.com/forms/d/e/${speakerFormId}/view
 export default ({ backgroundColor = theme.primary, page = `TORONTO` }: Props) => (
   <Container backgroundColor={backgroundColor}>
     <Header>
-      <Link href="http://slack.torontojs.com/" target="_blank">
+      <Link href='http://slack.torontojs.com/' target='_blank'>
         <SlackIcon style={{ width: `30px` }} />
         <LinkText>Join us on Slack</LinkText>
       </Link>
-      <Link href="http://meetup.com/torontojs" target="_blank">
-        <i className="fa fa-meetup" style={{ fontSize: `32px` }} />
+      <Link href='http://meetup.com/torontojs' target='_blank'>
+        <i className='fa fa-meetup' style={{ fontSize: `32px` }} />
         <LinkText>Join us on Meetup</LinkText>
       </Link>
-      <Link href="https://github.com/torontojs/torontojs.com" target="_blank">
-        <Octocat width="30px" />
+      <Link href='https://github.com/torontojs/torontojs.com' target='_blank'>
+        <Octocat width='30px' />
         <LinkText>Contribute to this site</LinkText>
       </Link>
-      <Link href="https://www.youtube.com/channel/UC1samyyfqiKmOT6fq3uVO1A" target="_blank">
-        <i className="fa fa-youtube-play" style={{ fontSize: `32px` }} />
+      <Link
+        href='https://www.youtube.com/channel/UC1samyyfqiKmOT6fq3uVO1A'
+        target='_blank'
+      >
+        <i className='fa fa-youtube-play' style={{ fontSize: `32px` }} />
         <LinkText>Tech Talks</LinkText>
       </Link>
-      <Link href={speakerFormLink} target="_blank">
-        <i className="fa fa-comment" style={{ fontSize: `32px` }} />
+      <Link href={speakerFormLink} target='_blank'>
+        <i className='fa fa-comment' style={{ fontSize: `32px` }} />
         <LinkText>Give a Talk</LinkText>
+      </Link>
+      <Link href='./p/code_of_conduct' target='_blank'>
+        <i className='fa fa-sticky-note' style={{ fontSize: `32px` }} />
+        <LinkText>Code of Conduct</LinkText>
       </Link>
     </Header>
     <InnerContainer>
       <TitleContainer>
-        <Title>{page}&nbsp;<b style={{ color: `#ffffff` }}>JS</b></Title>
+        <Title>
+          {page}&nbsp;<b style={{ color: `#ffffff` }}>JS</b>
+        </Title>
         <Rotator
           items={[`MEETUPS`, `TECH TALKS`, `WORKSHOPS`, `SOCIAL EVENTS`]}
         />
