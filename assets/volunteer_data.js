80e74141714b58fc9ad652e49e3e4abad6a2ccc4
dreymotoh <striclyspoken@gmail.com>
2023-11-07 14:56:17 -0500

    adjusted assets folder to have images folder and added volunteer_data file

---
+++ assets/volunteer_data.js
@@ -0,0 +1,239 @@
+const volunteer_data = {
+    contributors: [
+      {
+        name: “Jen Chan”,
+        profileURL: “https://www.jenchan.biz/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Kieran Huggins”,
+        profileURL: “https://kieran.ca/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Alex Wilmer”,
+        profileURL: “https://twitter.com/benevolentNinja”,
+        profileImageURL: “”,
+      },
+    ],
+    honorary_members: [
+      {
+        name: “Sherry Yang”,
+        profileURL: “https://5hel2l2y.github.io/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Erik Derohanian”,
+        profileURL: “https://www.linkedin.com/in/erikjd/”,
+        profileImageURL: “”,
+      },
+    ],
+    organizers: [
+      {
+        name: “Aman Aalam”,
+        profileURL: “https://www.linkedin.com/in/amanaalam/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Dale Karp”,
+        profileURL: “https://dale.io/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Evert Pot”,
+        profileURL: “https://evertpot.com/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Sami Xie”,
+        profileURL: “https://www.linkedin.com/in/sami-xie-91bb4814a”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Yuri Yang”,
+        profileURL: “https://www.linkedin.com/in/07yuri/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Marco Campos”,
+        profileURL: “https://madcampos.dev/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Ankur Kaushal”,
+        profileURL: “https://www.linkedin.com/in/ankur-kaushal/”,
+        profileImageURL: “”,
+      }
+    ],
+    directors: [
+      {
+        name: “Dann Toliver”,
+        profileURL: “https://danntoliver.com”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Matthew Mihok”,
+        profileURL: “https://twitter.com/mihok”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Jen Chan”,
+        profileURL: “https://www.jenchan.biz/”,
+        profileImageURL: “”,
+      },
+    ],
+    advisory: {
+      members: [
+        {
+          name: “Taz Singh”,
+          profileURL: “https://twitter.com/tazsingh”,
+          profileImageURL: “”,
+        },
+      ],
+    },
+    promotionModerationCommunityEngagement: [
+      {
+        name: “Alex Wilmer”,
+        profileURL: “https://twitter.com/benevolentNinja”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Andrew Matte”,
+        profileURL: “https://www.linkedin.com/in/andrew-matte”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Brandon Cha”,
+        profileURL: “https://www.linkedin.com/in/brandoncha/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Cam Remesz”,
+        profileURL: “https://www.linkedin.com/in/cameron-remesz/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Chris West”,
+        profileURL: “https://www.linkedin.com/in/chris-west-code-wrangler/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Divish Ram”,
+        profileURL: “https://www.linkedin.com/in/divish-ram-694b55230/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Danny Kim”,
+        profileURL: “https://www.linkedin.com/in/0916dhkim/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Drey Moreau”,
+        profileURL: “https://www.linkedin.com/in/dreymoreau/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Jack Li”,
+        profileURL: “https://www.linkedin.com/in/jackli0707/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Kaoru Tsumita”,
+        profileURL: “https://kaorut.com/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Ken Beaudin”,
+        profileURL: “https://www.linkedin.com/in/ken-beaudin-9a4061174/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Kieran Huggins”,
+        profileURL: “https://kieran.ca/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Lamisa Saadat”,
+        profileURL: “https://www.linkedin.com/in/lamisaadat/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Liz McReady”,
+        profileURL: “https://gingerkiwi.blog/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Matt J Jackson”,
+        profileURL: “https://www.linkedin.com/in/mattjacksondev/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Metkel Kebede”,
+        profileURL: “https://www.linkedin.com/in/metkel-kebede-50a79664/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Rajat Bansal”,
+        profileURL: “https://www.linkedin.com/in/rjtbansal/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Sammy Lam”,
+        profileURL:
+          “https://www.linkedin.com/in/sammy-lam-full-stack-dev-electrician-car-wrapper-it/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Zeinab Farag”,
+        profileURL: “https://www.linkedin.com/in/zeinab454/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Tehseen Chaudry”,
+        profileURL: “https://matcha.so/tehseen”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Zeinab Farag”,
+        profileURL: “https://www.linkedin.com/in/zeinab454/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Christine Hsiao”,
+        profileURL: “https://www.linkedin.com/in/christine-hsiao-dev/”,
+        profileImageURL: “”,
+      },
+    ],
+    educationalAccomplices: [
+      {
+        name: “Btara Truhandarien”,
+        profileURL: “https://btruhand.github.io/blog/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Connor Wilson”,
+        profileURL: “https://cwlsn.com/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “David Wolever”,
+        profileURL: “https://twitter.com/wolever”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Nicole Chung”,
+        profileURL: “https://www.linkedin.com/in/nicole-chung-6b06b33/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Niya Panamdanam”,
+        profileURL: “https://www.linkedin.com/in/niya-panamdanam/”,
+        profileImageURL: “”,
+      },
+      {
+        name: “Rachael Concessio”,
+        profileURL: “https://www.linkedin.com/in/rachaelconcessio/”,
+        profileImageURL: “”,
+      },
+    ],
+  };
